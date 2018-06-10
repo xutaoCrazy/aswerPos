bbc0bd2f1da2b887de12a77ca0c5d69d7e75482f
xutaoCrazy <1140077827@qq.com>
2018-06-10 23:03:11 +0800

    左侧收银栏和商品栏添加

--- src/components/page/Pos.vue
+++ src/components/page/Pos.vue
@@ -4,10 +4,26 @@
             <el-col :span='7' class="pos-bord" id='order-list'>
                 我是订单栏
             </el-col>
-            <el-col>
+            <el-col :span='17' id=''>
                 我是产品栏
             </el-col>
        </el-row>
+       <!-- <el-container id='demo'>
+        <el-aside width="200px">
+            <ul id='ulid'>
+                <li>我是导航栏一</li>
+                <li>我是导航栏一</li>
+                <li>我是导航栏一</li>
+                <li>我是导航栏一</li>
+                <li>我是导航栏一</li>
+            </ul>
+        </el-aside>
+        <el-container>
+          <el-header>Header</el-header>
+          <el-main>Main</el-main>
+          <el-footer>Footer</el-footer>
+        </el-container>
+      </el-container> -->
     </div>
 </template>
 <script>
@@ -15,7 +31,7 @@ export default {
   name: 'pos',
   mounted: function () {
     var oderheight = document.body.clientHeight
-    document.getElementById('order-list').style.height = oderheight + 'px'
+    document.getElementById('demo').style.height = oderheight + 'px'
   }
 }
 </script>
@@ -24,5 +40,43 @@ export default {
 .pos-bord{
     background-color: #f9fafc;
     border-right: 1px solid #c0ccda
+}
+.el-header, .el-footer {
+    background-color: #B3C0D1;
+    color: #333;
+    text-align: center;
+    line-height: 60px;
+  }
+  .el-aside {
+    background-color: #D3DCE6;
+    color: #333;
+    text-align: center;
+    line-height: 200px;
+  }
+  .el-main {
+    background-color: #E9EEF3;
+    color: #333;
+    text-align: center;
+    line-height: 160px;
+  }
+  body > .el-container {
+    margin-bottom: 40px;
+  }
+  .el-container:nth-child(5) .el-aside,
+  .el-container:nth-child(6) .el-aside {
+    line-height: 260px;
+  }
+  .el-container:nth-child(7) .el-aside {
+    line-height: 320px;
+  }
+#ulid{
+    padding: 0px;
+    margin: 0px;
+}
+#ulid li{
+    list-style: none;
+    text-align: center;
+    border-bottom: 1px solid #20a0ff;
+    padding:10px;
 }
 </style>
