01963423fd92bb04f51c649e38edcba88b9e1a05
xutaoCrazy <1140077827@qq.com>
2018-06-11 23:35:13 +0800

    表格制作

--- src/components/page/Pos.vue
+++ src/components/page/Pos.vue
@@ -2,7 +2,31 @@
     <div id='pos'>
        <el-row>
             <el-col :span='7' class="pos-bord" id='order-list'>
-                我是订单栏
+               <el-tabs >
+                 <el-tab-pane label='收银'>
+                   <el-table :data='tableData' border style='width:100'>
+                     <el-table-column prop="goodsName" label="商品名称" ></el-table-column>
+                     <el-table-column prop="count" label="数量" width='50' ></el-table-column>
+                     <el-table-column prop="price" label="单价" width='70'></el-table-column>
+                     <el-table-column  label="操作" fixed='right' width='100'>
+                       <template scope='scope'>
+                        <el-button type='text'  size='small'>删除</el-button>
+                        <el-button type='text'  size='samll'>增加</el-button>
+                       </template>
+                     </el-table-column>
+
+                   </el-table>
+                   <div class='div-btn'>
+                     <el-button type='warning'>挂单</el-button>
+                     <el-button type='danger'>删除</el-button>
+                     <el-button type='success'>结账</el-button>
+                   </div>
+                 </el-tab-pane>
+                 <el-tab-pane label='挂单'>
+                 </el-tab-pane>
+                 <el-tab-pane label='结账'>
+                 </el-tab-pane>
+               </el-tabs>
             </el-col>
             <el-col :span='17' id=''>
                 我是产品栏77
@@ -29,9 +53,33 @@
 <script>
 export default {
   name: 'pos',
+  data () {
+    return {
+      tableData: [{
+        goodsName: '可口可乐',
+        price: 8,
+        count: 1
+      },
+      {
+        goodsName: '香辣鸡腿堡',
+        price: 15,
+        count: 1
+      },
+      {
+        goodsName: '爱心薯条',
+        price: 8,
+        count: 1
+      },
+      {
+        goodsName: '甜筒',
+        price: 8,
+        count: 1
+      }]
+    }
+  },
   mounted: function () {
     var oderheight = document.body.clientHeight
-    document.getElementById('demo').style.height = oderheight + 'px'
+    document.getElementById('order-list').style.height = oderheight + 'px'
   }
 }
 </script>
@@ -41,34 +89,7 @@ export default {
     background-color: #f9fafc;
     border-right: 1px solid #c0ccda
 }
-.el-header, .el-footer {
-    background-color: #B3C0D1;
-    color: #333;
-    text-align: center;
-    line-height: 60px;
-  }
-  .el-aside {
-    background-color: #D3DCE6;
-    color: #333;
-    text-align: center;
-    line-height: 200px;
-  }
-  .el-main {
-    background-color: #E9EEF3;
-    color: #333;
-    text-align: center;
-    line-height: 160px;
-  }
-  body > .el-container {
-    margin-bottom: 40px;
-  }
-  .el-container:nth-child(5) .el-aside,
-  .el-container:nth-child(6) .el-aside {
-    line-height: 260px;
-  }
-  .el-container:nth-child(7) .el-aside {
-    line-height: 320px;
-  }
+
 #ulid{
     padding: 0px;
     margin: 0px;
@@ -78,5 +99,8 @@ export default {
     text-align: center;
     border-bottom: 1px solid #20a0ff;
     padding:10px;
+}
+.div-btn{
+  margin-top: 10px
 }
 </style>
