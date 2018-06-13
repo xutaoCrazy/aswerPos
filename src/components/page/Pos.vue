e92c7c4ab99b0f8af6b5c8c8e1cfc7aed23a1c7c
xutaoCrazy <1140077827@qq.com>
2018-06-14 00:14:17 +0800

    常用商品样式布局修改

--- src/components/page/Pos.vue
+++ src/components/page/Pos.vue
@@ -4,12 +4,12 @@
             <el-col :span='7' class="pos-bord" id='order-list'>
                <el-tabs >
                  <el-tab-pane label='收银'>
-                   <el-table :data='tableData' border style='width:100'>
-                     <el-table-column prop="goodsName" label="商品名称" ></el-table-column>
+                   <el-table :data='tableData' border style='width:200'>
+                     <el-table-column prop="goodsName" label="商品名称" style="margin-left:10px;" ></el-table-column>
                      <el-table-column prop="count" label="数量" width='50' ></el-table-column>
                      <el-table-column prop="price" label="单价" width='70'></el-table-column>
                      <el-table-column  label="操作" fixed='right' width='100'>
-                       <template scope='scope'>
+                       <template slot-scope='scope'>
                         <el-button type='text'  size='small'>删除</el-button>
                         <el-button type='text'  size='samll'>增加</el-button>
                        </template>
@@ -29,7 +29,35 @@
                </el-tabs>
             </el-col>
             <el-col :span='17' id=''>
-                我是产品栏77
+               <div class='often-goods'>
+                  <div class='title'>
+                     常用商品
+                  </div>
+                  <div class='often-goods-list'>
+                    <ul>
+                      <li v-for="often in offgoodsList" :key='often.goodsId'>
+                         <span>{{ often.goodsName }}</span>
+                         <span class='o-price'>￥{{ often.price }}元</span>
+                      </li>
+                    </ul>
+                  </div>
+               </div>
+               <div class='goods-type'>
+                <el-tabs>
+                  <el-tab-pane label='汉堡'>
+                    <ul class='cookList'>
+                      <li v-for='type in type0list' :key='type.goodsId'>
+                        <span class="foodImg"><img :src="type.goodsImg" width="100%"></span>
+                        <span class="foodName">{{ type.goodsName }}</span>
+                        <span class="foodPrice">￥{{ type.price }}元</span>
+                      </li>
+                    </ul>
+                  </el-tab-pane>
+                  <el-tab-pane label='小食'>小食</el-tab-pane>
+                  <el-tab-pane label='饮料'>饮料</el-tab-pane>
+                  <el-tab-pane label='套餐'>套餐</el-tab-pane>
+                </el-tabs>
+               </div>
             </el-col>
        </el-row>
        <!-- <el-container id='demo'>
@@ -55,7 +83,9 @@ export default {
   name: 'pos',
   data () {
     return {
-      tableData: ''
+      tableData: '',
+      offgoodsList: '',
+      type0list: ''
     }
   },
   mounted: function () {
@@ -63,28 +93,30 @@ export default {
     document.getElementById('order-list').style.height = oderheight + 'px'
   },
   methods: {
-    add () {
-      var url = "http://127.0.0.1:8088/get";
-      var $this = this;
-      this.$ajax({
-        method: "get",
-        url: url
-      }).then(function(res) {
-        console.log(res.data);
-        $this.tableData=res.data;
-      });
-    }
   },
-   created () {
-    var url = "http://127.0.0.1:8088/get";
-      var $this = this;
-      this.$ajax({
-        method: "get",
-        url: url
-      }).then(function(res) {
-        console.log(res.data);
-        $this.tableData=res.data;
-      });
+  created () {
+    var url = 'http://127.0.0.1:8088/get'
+    var urls = 'http://127.0.0.1:8088/gets'
+    var urlType = 'http://127.0.0.1:8088/gettype'
+    var $this = this
+    this.$ajax({
+      method: 'get',
+      url: url
+    }).then(function (res) {
+      $this.tableData = res.data
+    })
+    this.$ajax({
+      method: 'get',
+      url: urls
+    }).then(function (res) {
+      $this.offgoodsList = res.data
+    })
+    this.$ajax({
+      method: 'get',
+      url: urlType
+    }).then(function (res) {
+      $this.type0list = res.data
+    })
   }
 }
 </script>
@@ -108,4 +140,53 @@ export default {
 .div-btn{
   margin-top: 10px
 }
+.title{
+  height:20px;
+  border-bottom:1px solid #d3dce6;
+  background-color: #f9fafc;
+  padding: 10px;
+  text-align: left
+}
+.often-goods-list ul li{
+  list-style: none;
+  float:left;
+  border: 1px solid #E5e9fe;
+  padding: 10px;
+  margin: 10px;
+  background: #fff
+}
+.o-price{
+  columns: #5887ff
+}
+.goods-type{
+  clear: both;
+}
+.cookList li{
+       list-style: none;
+       width:23%;
+       border:1px solid #E5E9F2;
+       height: auot;
+       overflow: hidden;
+       background-color:#fff;
+       padding: 2px;
+       float:left;
+       margin: 2px;
+   }
+   .cookList li span{
+        display: block;
+        float:left;
+   }
+   .foodImg{
+       width: 40%;
+   }
+   .foodName{
+       font-size: 18px;
+       padding-left: 10px;
+       color:brown;
+   }
+   .foodPrice{
+       font-size: 16px;
+       padding-left: 10px;
+       padding-top:10px;
+   }
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -2,8 +2,6 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import pos from '@/components/page/Pos'
 import axios from 'axios'
-
-
 Vue.use(Router)
 Vue.prototype.$ajax = axios
 
